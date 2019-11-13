4f0fbc1e9e45bcb938aeff7187eee9d467d0eeb0
kmh339 <kmh339@gmail.com>
2019-11-13 14:33:08 +0900

    ee

--- app/src/main/java/com/example/testing/MainActivity.java
+++ app/src/main/java/com/example/testing/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.testing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//test
 public class MainActivity extends AppCompatActivity {
 
     @Override
